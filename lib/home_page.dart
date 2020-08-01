3855aa55deec8b259bc01395a9b8bde2693a1f2e
avlearner <iamaryanve18@gmail.com>
2020-08-01 20:12:21 +0530

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+
+void Buttonp(){
+  RaisedButton(
+    onPressed: (){},
+    child: Text("1"),
+  );}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() => runApp(MaterialApp(
+title: "Calculator",
+theme: ThemeData(primarySwatch: Colors.green),
+home:Homepage(),), );
+
