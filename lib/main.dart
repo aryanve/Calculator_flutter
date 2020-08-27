76842cfa073757c39107b5e32376731b5bc35746
Aryan Verma <64201232+aryanve@users.noreply.github.com>
2020-08-27 20:35:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 void main() => runApp(MaterialApp(
-title: "Calculator",
-debugShowCheckedModeBanner: false,
-theme: ThemeData(brightness: Brightness.light),
-home:Cal(),
+  title: "Calculator",
+  debugShowCheckedModeBanner: false,
+  home:Cal(),
 ), );
 
 class Cal extends StatefulWidget {
@@ -14,17 +13,24 @@ class Cal extends StatefulWidget {
 }
 
 class _CalState extends State<Cal> {
+
+  Widget button(String buttonText, double buttonWidth, Color buttonColor){
+    return Container(
+      width: MediaQuery.of(context).size.width* 0.1 *buttonWidth,
+      child: RaisedButton(
+          padding: EdgeInsets.all(20),
+          color: buttonColor,
+          onPressed:() => buttonPressed(buttonText),
+          child:Text(buttonText, style: TextStyle(fontSize: 40,color: Colors.black),)
+      ) ,
+    );
+  }
   String equation ="0";
   String result="0";
   String expression ="";
   double equationFontSize =36;
   double resultFontSize =51;
 
-  buttonPressedlong(){
-    setState(() {
-        equation = equation + ".";
-      });
-  }
   buttonPressed(String buttonText){
     setState(() {
       if(buttonText == "C"){
@@ -47,7 +53,7 @@ class _CalState extends State<Cal> {
       } else if(buttonText == "="){
         equationFontSize =36;
         resultFontSize =52;
-        
+
         expression = equation;
         expression = expression.replaceAll('×', '*');
         expression = expression.replaceAll('÷', '/');
@@ -55,7 +61,7 @@ class _CalState extends State<Cal> {
         try{
           Parser p= new Parser();
           Expression exp =p.parse(expression);
-          
+
           ContextModel cm = ContextModel();
           result = '${exp.evaluate(EvaluationType.REAL, cm)}';
         }catch(e){
@@ -66,24 +72,11 @@ class _CalState extends State<Cal> {
         resultFontSize =36;
         if(equation=="0"){
           equation= buttonText;}
-          else {
+        else {
           equation = equation + buttonText;
         }
       }
     });
-  }
-
-  Widget button(String buttonText, double buttonWidth, Color buttonColor){
-    return Container(
-      width: MediaQuery.of(context).size.width* 0.1 *buttonWidth,
-      child: RaisedButton(
-          padding: EdgeInsets.all(20),
-          color: buttonColor,
-          onPressed:() => buttonPressed(buttonText),
-          onLongPress: () => buttonPressedlong(),
-          child:Text(buttonText, style: TextStyle(fontSize: 40,color: Colors.black),)
-      ) ,
-    );
   }
 
   @override
@@ -103,66 +96,81 @@ class _CalState extends State<Cal> {
             child: Text(result, style: TextStyle(fontSize: resultFontSize),),
           ),
 
-         Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                   Container(
-                     width: MediaQuery.of(context).size.width* .5,
-                     child: RaisedButton(
-                       padding: EdgeInsets.all(7),
-                       onPressed: () => buttonPressed("C"),
-                       color: Colors.purple[900],
-                       child: Text("C",style: TextStyle(fontSize: 34,color: Colors.white)),
-                     ),
-                   ),
-                    Container(
-                      width: MediaQuery.of(context).size.width* .5,
-                      child: RaisedButton(
-                        padding: EdgeInsets.all(7),
-                        onPressed: () => buttonPressed("⌫"),
-                        color: Colors.red[900],
-                        child: Text("⌫",style: TextStyle(fontSize: 29.7,color: Colors.white)),
-                      ),
-                    )
-                  ],
-                ),
-
-                  Row(
-                   children: <Widget>[
-                     button("1", 2.5,Colors.cyanAccent),
-                     button("2", 2.5 ,Colors.cyanAccent),
-                     button("3", 2.5 ,Colors.cyanAccent),
-                     button("+", 2.5 ,Colors.pinkAccent),
-                   ],
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width* .5,
+                    child: RaisedButton(
+                      padding: EdgeInsets.all(7),
+                      onPressed: () => buttonPressed("C"),
+                      color: Colors.purple[900],
+                      child: Text("C",style: TextStyle(fontSize: 34,color: Colors.white)),
+                    ),
                   ),
-                  Row(
-                      children: <Widget>[
-                        button("4", 2.5 ,Colors.cyanAccent),
-                        button("5", 2.5 ,Colors.cyanAccent),
-                        button("6", 2.5 ,Colors.cyanAccent),
-                        button("−", 2.5 ,Colors.pinkAccent),
-                      ],
-                  ),
-                Row(
-                  children: <Widget>[
-                    button("7", 2.5 ,Colors.cyanAccent),
-                    button("8", 2.5,Colors.cyanAccent),
-                    button("9", 2.5 ,Colors.cyanAccent),
-                    button("×", 2.5 ,Colors.pinkAccent),
+                  Container(
+                    width: MediaQuery.of(context).size.width* .5,
+                    child: RaisedButton(
+                      padding: EdgeInsets.all(7),
+                      onPressed: () => buttonPressed("⌫"),
+                      color: Colors.red[900],
+                      child: Text("⌫",style: TextStyle(fontSize: 29.7,color: Colors.white)),
+                    ),
+                  )
+                ],
+              ),
 
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    button("0.", 2.5 ,Colors.cyanAccent),
-                    button("=", 5,Colors.pinkAccent),
-                    button("÷", 2.5 ,Colors.pinkAccent),
-                  ],
-                ),
-              ],),
-      ],
-    ),
+              Row(
+                children: <Widget>[
+                  button("1", 2.5 ,Colors.cyanAccent),
+                  button("2", 2.5 ,Colors.cyanAccent),
+                  button("3", 2.5 ,Colors.cyanAccent),
+                  button("+", 2.5 ,Colors.pinkAccent),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  button("4", 2.5 ,Colors.cyanAccent),
+                  button("5", 2.5 ,Colors.cyanAccent),
+                  button("6", 2.5 ,Colors.cyanAccent),
+                  button("−", 2.5 ,Colors.pinkAccent),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  button("7", 2.5 ,Colors.cyanAccent),
+                  button("8", 2.5 ,Colors.cyanAccent),
+                  button("9", 2.5 ,Colors.cyanAccent),
+                  button("×", 2.5 ,Colors.pinkAccent),
+
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(15.5),
+                    color: Colors.cyanAccent,
+                    width: MediaQuery.of(context).size.width* 0.1 *2.5,
+                    child: GestureDetector(
+                      onTap: () =>
+                        setState((){
+                          equation = equation + "0";
+                        }),
+                      onDoubleTap: () =>
+                          setState(() {
+                            equation = equation + ".";
+                          }),
+                      child: Text(" 0 ." , style: TextStyle(fontSize: 48,color: Colors.black))
+                    ),
+                  ),
+                  button("=", 5 ,Colors.pinkAccent),
+                  button("÷", 2.5 ,Colors.pinkAccent),
+                ],
+              ),
+            ],),
+        ],
+      ),
     );
   }
 }
